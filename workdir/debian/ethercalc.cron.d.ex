8a7de8e20445aee69de9fa77b1d0a2a0e8e58685
lafont <francois.lafont@ac-versailles.fr>
2018-01-27 17:55:03 +0100

    Sat Jan 27 17:55:03 CET 2018: Update

--- workdir/debian/ethercalc.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the ethercalc package
-#
-0 4	* * *	root	[ -x /usr/bin/ethercalc_maintenance ] && /usr/bin/ethercalc_maintenance
--- workdir/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(ethercalc):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="ethercalc" command="/usr/bin/ethercalc"
